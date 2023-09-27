5e56cf849686f8eec01e7ad2d63ec0fbd33618e1
Berfin <100244265+berfynkaia@users.noreply.github.com>
2023-09-27 17:50:29 +0300

    Add files via upload

---
+++ 27thsept.py
@@ -0,0 +1,16 @@
+# theString = "berfynkaia"
+# print(len(theString))
+# print(theString[0:4])
+# print(theString[-4:])
+# print(theString[0:4] , theString[-4:])
+# reverso = theString[10::-1] #or [::-1]
+# print(reverso)
+
+
+x = input("Create a password: ")
+y = x.isalpha
+
+if True:
+  print("That's a valid password.")
+else:
+  print("Use the alphabet. Try again.")
