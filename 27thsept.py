aff3ff85d3d43a40ff91ee388672ce2e745c35b4
Berfin <100244265+berfynkaia@users.noreply.github.com>
2023-09-27 21:45:36 +0300

    Update 27thsept.py

--- 27thsept.py
+++ 27thsept.py
@@ -15,3 +15,22 @@ if True:
 else:
   print("Use the alphabet. Try again.")
 
+
+
+
+name = input("enter your name: ")
+def greet(name):
+  print(f"Hello, {name}. I hope you are doing well.") 
+greet(name)
+
+
+
+
+
+age = input("enter your age: ")
+age = int(age)
+
+def calculate(age):
+  userAge = (2023 - age)
+  print(f"You were born in {userAge}.")
+calculate(age)
