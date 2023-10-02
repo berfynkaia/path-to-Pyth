e0897fee27f0697ea22679fc08998759dbe5f2d0
Berfin <100244265+berfynkaia@users.noreply.github.com>
2023-10-02 03:03:39 +0300

    Add files via upload

---
+++ 2ndocto2.py
@@ -0,0 +1,24 @@
+
+name = input("Enter your name: ")
+print("your password should be composed of all numbers expressed in 4 digits.")
+password = input("enter your password: ")
+y = password.isalnum()
+
+if (y == True) and (len(password) == 4):
+    print("great.")
+else:
+    print("not going to work.")
+age = (input("Your age: "))
+z = age.isalnum()
+if (z == True) and (int(age) >= 18):
+    print("You are old enough to be a member.")
+else:
+    print("Check your input.")
+user = [name, password, age]
+print(f"You are {name}. Your password is ****, and you are {age} years old.")
+
+
+
+    
+
+
